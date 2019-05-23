c0f44652bd1a77afb7b16c26bdb25bf0b5d6cf2e
Akira Osanai <50911016+Akira-Osanai@users.noreply.github.com>
2019-05-23 21:17:03 +0900

    Update RandomMenberSelecter.java

--- RandomMenberSelecter.java
+++ RandomMenberSelecter.java
@@ -6,8 +6,8 @@ public class RandomMenberSelecter {
 		ArrayList <Integer> menber = new ArrayList <>();
 		System.out.println("乱数による人数の割り振りを実行します。");
 					//////////////////////////////////////
-		int capacity  = 10; 	// この文の数値を定員の数に変更する	   //
-		int Applicant = 30; 	// この文の数値を希望者の数に変更する	  //
+		int capacity  = 10; 	// この文の数値を定員の数に変更する	    //
+		int Applicant = 30; 	// この文の数値を希望者の数に変更する	    //
 					//////////////////////////////////////
 		for (int j = 0; j < capacity ; j++)
 			menber.add(j, 1 + (int) (Math.random() * Applicant));
