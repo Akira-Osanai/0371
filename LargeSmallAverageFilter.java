a5453a53bb7f9bc057829d3737c0ece17c6aa4c3
Akira Osanai <50911016+Akira-Osanai@users.noreply.github.com>
2019-05-28 21:08:49 +0900

    Update LargeSmallAverageFilter.java

--- LargeSmallAverageFilter.java
+++ LargeSmallAverageFilter.java
@@ -7,14 +7,14 @@ public class LargeSmallAverageFilter {
 		int SmallestNumber = 0;
 		int AverageNumber = 0;
 
-		int array = 10;													// 配列数
-		int ElementCount = 1000;										// 設定値-1
+		int array = 10;								// 配列数
+		int ElementCount = 1000;						// 設定値-1
 
 		ArrayList<Integer> number = new ArrayList<>();
 		for (int i = 0; i < array; i++) {
 //			number.add(i, 1 + (int) (Math.random() * ElementCount));	// 要素の値がランダム
-//			number.add(i);												// 要素の値が0からarrayへ
-			number.add(array-i);										// 要素の値がarrayから0へ
+//			number.add(i);							// 要素の値が0からarrayへ
+			number.add(array-i);						// 要素の値がarrayから0へ
 
 			// 最大数を検索
 			if(LargestNumber == 0) {
@@ -38,12 +38,12 @@ public class LargeSmallAverageFilter {
 	}
 }
 //////要素の値が小さい順にしようとしたけど、要素の始まりの配列番号を指定していないのに、最後だけ指定してるから実行時エラー吐く。 //
-//for (i = array; i >= 0; i--) {																			                            //
-//		number.add(i, 1 + (int) (Math.random() * ElementCount));											//
-//		number.add(i);																						//
-//																											//
-//	Exception in thread "main" java.lang.IndexOutOfBoundsException: Index: 10, Size: 1						//
-//	at java.util.ArrayList.rangeCheck(ArrayList.java:657)													//
-//	at java.util.ArrayList.get(ArrayList.java:433)															//
-//	at LargeSmallAverageFilter.main(LargeSmallAverageFilter.java:24)										//
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//for (i = array; i >= 0; i--) {								   //
+//		number.add(i, 1 + (int) (Math.random() * ElementCount));			   //
+//		number.add(i);									   //
+//												   //
+//	Exception in thread "main" java.lang.IndexOutOfBoundsException: Index: 10, Size: 1	   //
+//	at java.util.ArrayList.rangeCheck(ArrayList.java:657)				   	   //
+//	at java.util.ArrayList.get(ArrayList.java:433)			  			   //
+//	at LargeSmallAverageFilter.main(LargeSmallAverageFilter.java:24)		           //
+/////////////////////////////////////////////////////////////////////////////////////////////////////
